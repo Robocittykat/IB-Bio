937289792a25acf58691963ee1fbf1e0f3037ad4
Robocittykat <115361685+Robocittykat@users.noreply.github.com>
2026-01-05 10:05:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ for(let i = 0; i<18; i++){
 	moths = moths.concat(new moth(new genotype(0,0),{x:Math.floor(Math.random()*6),y:Math.floor(Math.random()*6)}))
 }
 
-console.value += forest
+console.innerHTML += forest
 
 function simulationTick(){
 	for(let org of moths){
