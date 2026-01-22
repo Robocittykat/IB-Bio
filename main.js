ebe8fcb257c7f51c2495e9701eafded25f9cc64e
Robocittykat Chromebook <natethegreatjoseph@gmail.com>
2026-01-22 18:47:39 -0500

    fully implements baseline and features A and B

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
 let features = {
-    a:false, //large population
+    a:true, //large population
     b:true, //accurate genetics
     c:false, //random reproduction
     d:false, //age mechanic
     e:false, //complexified predators
-    trees:0,
+    trees:1,
 }
+
+const MutationRate = 0.000001
+const ShownPredRate = 0.122
+const CamoPredRate = 0.111
+const StartRatio = 0.000005 //chance of dom gene
+ 
+//expectation: (from https://sites.lsa.umich.edu/globalchange/labs/natural-selection-and-mutation-the-case-of-the-peppered-moth/)
+//1750 - 0.00001 (not sampled because starting dist)
+//1850 - 0.01
+//1950 - 0.9
+
+
+//Shown moths die 1.1x as often (Majerus)
+//Moths live 9 months, doing the math we get that there's about 1/9 chance of dying
+//      per month
+//Thus camo death rate is 0.111, shown is 0.122
+
+
+let year = 1750
+let month = 0
+
+
 
 class genotype{
     constructor(...gene){
@@ -30,14 +52,20 @@ class moth{
 }
 
 
-
+function getRandomGene(){
+    if(Math.random() < StartRatio){
+	return 1
+    }else{
+	return 0
+    }
+}
 
 let moths = []
-for(let i = 0; i<(10*100**features.a); i++){
+for(let i = 0; i<(10*1000**features.a); i++){
     if(!features.b){
-        moths = moths.concat(new moth(g(Math.floor(Math.random()*2))))
+        moths = moths.concat(new moth(g(getRandomGene())))
     }else{
-	moths = moths.concat(new moth(g(Math.floor(Math.random()*2),Math.floor(Math.random()*2))))
+	moths = moths.concat(new moth(g(getRandomGene(),getRandomGene())))
     }
 }
 
@@ -47,11 +75,11 @@ function simulationTick(){
     let killedMoths = []
     for(let org of moths){
 	if(org.geno.color == features.trees){
-	    if(Math.random() < 0.01){
+	    if(Math.random() < CamoPredRate){
 		killedMoths = killedMoths.concat(org)
 	    }
 	}else{
-	    if(Math.random() < 0.2){
+	    if(Math.random() < ShownPredRate){
 		killedMoths = killedMoths.concat(org)
 	    }
 	}
@@ -64,7 +92,7 @@ function simulationTick(){
 	let genes
 	if(!features.b){
 	    genes = moths[Math.floor(Math.random()*moths.length)].geno.color
-	    if(Math.random() < 0.1){
+	    if(Math.random() < MutationRate){
 	        genes = Number(!genes)
 	    }
 	}else{
@@ -73,10 +101,22 @@ function simulationTick(){
 
 	    let g1 = p1.geno.genes[Math.floor(Math.random()*2)]
 	    let g2 = p2.geno.genes[Math.floor(Math.random()*2)]
+
+	    if(Math.random() < MutationRate){
+		g1 = Number(!g1)
+	    }if(Math.random() < MutationRate){
+		g2 = Number(!g2)
+	    }
 	    genes = [g1,g2]
         }
-	newMoths = newMoths.concat(new moth(g(genes)))
+	newMoths = newMoths.concat(new moth(g(...genes)))
     }moths = moths.concat(newMoths)
+
+    month ++
+    if(month >= 12){
+	year ++
+	month = 0
+    }
 
     mothInfo()
 }
@@ -107,8 +147,12 @@ function mothInfo(){
     }
     display.max = moths.length
     display.value=light
+    time.innerHTML = month+"/"+year
 }
 
 mothInfo()
 
-//setInterval(simulationTick,100)
+//let ticker = setInterval(simulationTick,100)
+function stop(){
+    clearInterval(ticker)
+}
